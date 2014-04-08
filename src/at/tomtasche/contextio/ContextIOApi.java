158000fed4ea99be050f78dd6ea4b89ce8d507b1
Neal Sorensen <nealvs@gmail.com>
2014-04-07 23:25:04 -0700

    Formatting and moved libs.

--- src/at/tomtasche/contextio/ContextIOApi.java
+++ src/at/tomtasche/contextio/ContextIOApi.java
@@ -5,18 +5,18 @@ import org.scribe.model.Token;
 
 public class ContextIOApi extends DefaultApi10a {
 
-	@Override
-	public String getAccessTokenEndpoint() {
-		throw new UnsupportedOperationException("No endpoint for access token.");
-	}
+    @Override
+    public String getAccessTokenEndpoint() {
+        throw new UnsupportedOperationException("No endpoint for access token.");
+    }
 
-	@Override
-	public String getAuthorizationUrl(Token arg0) {
-		throw new UnsupportedOperationException("No authorization url.");
-	}
+    @Override
+    public String getAuthorizationUrl(Token arg0) {
+        throw new UnsupportedOperationException("No authorization url.");
+    }
 
-	@Override
-	public String getRequestTokenEndpoint() {
-		throw new UnsupportedOperationException("No endpoint for request token.");
-	}
+    @Override
+    public String getRequestTokenEndpoint() {
+        throw new UnsupportedOperationException("No endpoint for request token.");
+    }
 }
